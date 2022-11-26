c52e13ed1a7c00dc776f2eed9b900fe95db5e6d8
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-11-26 14:46:33 +0000

    Sorted tasks by when added

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,4 +1,6 @@
 <script>
+import { ref } from "vue";
+
 export default {
   data() {
     return {
@@ -8,28 +10,42 @@ export default {
         {
           name: "Feed cat",
           priority: 2,
+          id: 1,
         },
         {
           name: "Take out rubbish",
           priority: 8,
+          id: 2,
         },
         {
           name: "Buy mum's birthday present",
           priority: 1,
+          id: 3,
         },
       ],
     };
   },
 
   methods: {
-    submitTask(e) {
+    submitTask() {
       if (this.task.length === 0) return;
       if (this.priority < 1 || this.priority > 10) return;
 
       this.tasks.push({
         name: this.task,
         priority: this.priority,
+        id: this.tasks[this.tasks.length - 2].id + 1,
       });
+    },
+    orderNumerically(e) {
+      const sortedArray = this.tasks.sort((a, b) => a.priority - b.priority);
+
+      this.tasks = sortedArray;
+    },
+    orderWhenAdded() {
+      // Revert array back to what is was before
+      const sortById = this.tasks.sort((a, b) => a.id - b.id);
+      this.tasks = sortById;
     },
   },
 };
@@ -45,16 +61,17 @@ export default {
       <input v-model="priority" id="priority" type="number" min="1" max="10" />
       <button @click="submitTask" type="submit">Create task</button>
     </div>
+    <button @click="orderNumerically">Sort by priority</button>
+    <button @click="orderWhenAdded">Sort by when added</button>
     <ul>
-      <li v-for="item of tasks">{{ item.name }} {{ item.priority }}</li>
+      <li v-for="item of tasks">{{ item.name }}</li>
     </ul>
   </div>
 </template>
 
-<!-- Expected Behaviours: - The ability to add a new task, with a priority of between
-1-10. - The ability to view a list of all the tasks - The ability to order the
-tasks by priority or the order they were added to the list - The ability to
-delete a task - (Optional) The ability to filter to tasks below a certain
+<!--
+ The ability to order the tasks by priority or the order they were added to the list - The ability to delete a task - 
+(Optional) The ability to filter to tasks below a certain
 priority (e.g. display only tasks with a priority below 3) You may add
 additional features if you have the time. -->
 <style scoped></style>
